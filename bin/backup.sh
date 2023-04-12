295c22bd33c635b52e80c23ed1906bfa2c548969
Hugh Smalley <hugh.smalley@lexisnexisrisk.com>
2023-04-12 11:58:13 -0400

    Work Desktop Setup pt1

---
+++ bin/backup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+#Set Vars
+set -a
+. ${HOME}/.config/restic.env
+set +a
+
+restic backup --verbose --tag systemd.timer --tag hughmac --exclude-file=${HOME}/.config/restic.exclude $BACKUP_PATHS
+#/opt/homebrew/bin/restic forget --verbose --tag systemd.timer --tag hughmac --group-by "paths,tags" --keep-daily $RETENTION_DAYS --keep-weekly $RETENTION_WEEKS --keep-monthly $RETENTION_MONTHS --keep-yearly $RETENTION_YEARS
+restic cache --cleanup
