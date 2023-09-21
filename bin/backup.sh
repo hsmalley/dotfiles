2fe262b24d51c20ad4c6ff6036d7d73a033f7695
Hugh Smalley <hugh.smalley@lexisnexisrisk.com>
2023-09-21 14:33:11 -0400

    better retention

--- bin/backup.sh
+++ bin/backup.sh
@@ -6,5 +6,6 @@ set -a
 set +a
 
 restic backup --verbose --tag systemd.timer --tag $(hostname -s) --exclude-file=${HOME}/.config/restic.exclude $BACKUP_PATHS
-restic forget --verbose --tag systemd.timer --tag $(hostname -s) --group-by "paths,tags" --keep-daily $RETENTION_DAYS --keep-weekly $RETENTION_WEEKS --keep-monthly $RETENTION_MONTHS --keep-yearly $RETENTION_YEARS
+#restic forget --verbose --tag systemd.timer --tag $(hostname -s) --group-by "paths,tags" --keep-daily $RETENTION_DAYS --keep-weekly $RETENTION_WEEKS --keep-monthly $RETENTION_MONTHS --keep-yearly $RETENTION_YEARS
+restic forget --verbose --tag systemd.timer --tag $(hostname -s) --group-by "paths,tags" --keep-within-daily 7d --keep-within-monthly 1m --keep-within-yearly 1y
 restic cache --cleanup
